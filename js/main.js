4efed2bc471a2c1038dd14f26e22c2cefb552eb8
Maksim <kh.stomaks@gmail.com>
2018-03-29 20:26:25 +0300

    test commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// window.onload = function(){
+//   var btn = document.getElementById('btn');
+//   btn.onclick = function(){
+//     var prise = parseInt(document.getElementById('prise').innerHTML);
+//     var col = parseInt(document.getElementById('col').value);
+//     var totalAmount = col * prise;
+//     document.getElementById('valuePrise').append(totalAmount + '$');
+//   };
+// };
+// $('.parallax-window').parallax({imageSrc: "picture/topFon1.jpg"});
